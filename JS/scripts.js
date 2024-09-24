e20519b113fc8915af892424ee5d2d918464b3c8
stefv21 <161451930+stefv21@users.noreply.github.com>
2024-09-24 17:49:39 -0400

    updated html and js

---
+++ JS/scripts.js
@@ -0,0 +1,26 @@
+function newItem (){
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+  
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      $('#list').append(li);
+    }
+
+
+    function crossOut() {
+		li.toggleClass("strike");
+	}
+
+	li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    $('#list').sortable();
+}
