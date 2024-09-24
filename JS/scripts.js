d421d80eea59b60256bfa5795ad767be5e0b65a8
stefv21 <161451930+stefv21@users.noreply.github.com>
2024-09-24 19:13:14 -0400

    updated css and js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,26 +1,28 @@
-function newItem (){
-    let li = $('<li></li>');
-    let inputValue = $('#input').val();
-    li.append(inputValue);
-  
+function newItem() {
+    let inputValue = $('#input').val().trim(); // Trim whitespace
+
     if (inputValue === '') {
-      alert("You must write something!");
+        alert("You must write something!");
     } else {
-      $('#list').append(li);
+        // Create the list item and button with jQuery
+        let li = $('<li></li>').text(inputValue);
+        let deleteButton = $('<button class="crossOutButton"></button>').text('X');
+        
+        // Append the button to the list item
+        li.append(deleteButton);
+
+        // Append the list item to the list
+        $('#list').append(li);
+        
+        // Clear the input field after adding
+        $('#input').val('');
+
+        // Make the list sortable
+        $('#list').sortable();
+
+        // Event handler to remove the item
+        deleteButton.on("click", function() {
+            li.remove(); // Remove the list item when the button is clicked
+        });
     }
-
-
-    function crossOut() {
-		li.toggleClass("strike");
-	}
-
-	li.on("dblclick", function crossOut() {
-		li.toggleClass("strike");
-	});
-
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode('X'));
-    li.append(crossOutButton);
-
-    $('#list').sortable();
 }
